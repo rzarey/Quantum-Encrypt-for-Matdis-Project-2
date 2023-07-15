775164328f341ad2e275573b629f3f1cebf11d12
rzarey <reyhanssan0099@gmail.com>
2023-07-15 14:20:36 +0700

    update final project

---
+++ Final_projecy.py
@@ -0,0 +1,154 @@
+from qiskit import QuantumCircuit, transpile, assemble, Aer, execute
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
+
+def caesar_encrypt(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def text_to_binary(text):
+    binary_string = ""
+    for char in text:
+        binary_char = format(ord(char), '08b')
+        binary_string += binary_char
+    return binary_string
+
+def binary_to_text(binary):
+    text = ""
+    for i in range(0, len(binary), 8):
+        binary_char = binary[i:i+8]
+        decimal_char = int(binary_char, 2)
+        char = chr(decimal_char)
+        text += char
+    return text
+
+def get_key_from_cesar(shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    key = shifted_alphabet[0]
+    for char in shifted_alphabet:
+        if shifted_alphabet.count(char) > shifted_alphabet.count(key):
+            key = char
+    key_binary = format(ord(key), '08b')
+    return key_binary
+
+def most_frequent_bit(key):
+    count_0 = 0
+    count_1 = 0
+    for bit in key:
+        if bit == '0':
+            count_0 += 1
+        elif bit == '1':
+            count_1 += 1
+    if count_0 > count_1:
+        return '0'
+    else:
+        return '1'
+
+def encrypt_message(message, key):
+    circuit = QuantumCircuit(len(message), len(message))
+    for i, bit in enumerate(message):
+        if bit == '1':
+            circuit.x(i)
+    for i, bit in enumerate(key):
+        if bit == '1':
+            circuit.h(i)
+    circuit.measure(range(len(message)), range(len(message)))
+    simulator = Aer.get_backend('qasm_simulator')
+    job = execute(circuit, simulator, shots=1)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    encrypted_message = list(counts.keys())[0]
+    return encrypted_message
+
+def decrypt_message(encrypted_message, key_qubit):
+    circuit = QuantumCircuit(len(encrypted_message), len(encrypted_message))
+    for i, bit in enumerate(key_qubit):
+        if bit == '1':
+            circuit.h(i)
+    for i, bit in enumerate(encrypted_message):
+        if bit == '1':
+            circuit.x(i)
+    circuit.measure(range(len(encrypted_message)), range(len(encrypted_message)))
+    simulator = Aer.get_backend('qasm_simulator')
+    job = execute(circuit, simulator, shots=1)
+    result = job.result()
+    counts = result.get_counts(circuit)
+    decrypted_message = list(counts.keys())[0]
+    return decrypted_message
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # self.setStyleSheet("background-color: grey; color: green;")
+        self.setWindowTitle('Quantum Caesar Cipher')
+
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        self.label = QLabel('Masukkan teks:')
+        vbox.addWidget(self.label)
+
+        self.textEdit = QTextEdit()
+        vbox.addWidget(self.textEdit)
+
+        self.labelShift = QLabel('Masukkan jumlah pergeseran:')
+        vbox.addWidget(self.labelShift)
+
+        self.lineEditShift = QLineEdit()
+        vbox.addWidget(self.lineEditShift)
+
+        self.btnEncrypt = QPushButton('Enkripsi', self)
+        self.btnEncrypt.clicked.connect(self.encrypt)
+        vbox.addWidget(self.btnEncrypt)
+
+        self.btnDecrypt = QPushButton('Dekripsi', self)
+        self.btnDecrypt.clicked.connect(self.decrypt)
+        vbox.addWidget(self.btnDecrypt)
+
+        self.labelResult = QLabel('Hasil:')
+        vbox.addWidget(self.labelResult)
+
+        self.textEditResult = QTextEdit()
+        vbox.addWidget(self.textEditResult)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    def encrypt(self):
+        text = self.textEdit.toPlainText()
+        shift = int(self.lineEditShift.text())
+        encrypted_text = caesar_encrypt(text, shift)
+        binary = text_to_binary(encrypted_text)
+        key = get_key_from_cesar(shift)
+        key_qubit = most_frequent_bit(key)
+        encrypted_message = encrypt_message(binary, key)
+        self.textEditResult.setText(encrypted_message)
+
+    def decrypt(self):
+        encrypted_message = self.textEdit.toPlainText()
+        shift = int(self.lineEditShift.text())
+        key = get_key_from_cesar(shift)
+        key_qubit = most_frequent_bit(key)
+        decrypted_message = decrypt_message(encrypted_message, key_qubit)
+        text = binary_to_text(decrypted_message)
+        decrypted_text = caesar_encrypt(text, -shift)
+        self.textEditResult.setText(decrypted_text)
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
--- try_dec.py
+++ try_dec.py
@@ -1,33 +1,38 @@
 from qiskit import QuantumCircuit, transpile, assemble, Aer, execute
 
-def caesar_decrypt(text, shift):
-    decrypted_text = ""
-    for char in text:
-        if char.isalpha():
-            if char.islower():
-                decrypted_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
-            else:
-                decrypted_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
-            decrypted_text += decrypted_char
-        else:
-            decrypted_text += char
-    return decrypted_text
+def get_key_from_cesar(shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    key = shifted_alphabet[0]
+    for char in shifted_alphabet:
+        if shifted_alphabet.count(char) > shifted_alphabet.count(key):
+            key = char
+    key_binary = format(ord(key), '08b')
+    return key_binary
 
-def binary_to_text(binary):
-    text = ""
-    for i in range(0, len(binary), 8):
-        binary_char = binary[i:i+8]  # Mengambil 8 bit biner untuk setiap karakter
-        decimal_char = int(binary_char, 2)  # Mengonversi biner menjadi bilangan desimal
-        char = chr(decimal_char)  # Mengonversi bilangan desimal ke karakter
-        text += char
-    return text
+def most_frequent_bit(key):
+    count_0 = 0
+    count_1 = 0
+    
+    for bit in key:
+        if bit == '0':
+            count_0 += 1
+        elif bit == '1':
+            count_1 += 1
+    
+    if count_0 > count_1:
+        return '0'
+    else:
+        return '1'
+    
 
-def decrypt_message(encrypted_message, key):
+# Fungsi untuk mendekripsi pesan
+def decrypt_message(encrypted_message, key_qubit):
     # Inisialisasi sirkuit kuantum dengan panjang pesan terenkripsi
     circuit = QuantumCircuit(len(encrypted_message), len(encrypted_message))
 
     # Melewati kunci melalui qubit
-    for i, bit in enumerate(key):
+    for i, bit in enumerate(key_qubit):
         if bit == '1':
             circuit.h(i)
 
@@ -50,26 +55,44 @@ def decrypt_message(encrypted_message, key):
 
     return decrypted_message
 
-# Memasukkan pesan terenkripsi dan pergeseran yang digunakan saat enkripsi
+
+
+def binary_to_text(binary):
+    text = ""
+    for i in range(0, len(binary), 8):
+        binary_char = binary[i:i+8]  # Mengambil 8 bit biner untuk setiap karakter
+        decimal_char = int(binary_char, 2)  # Mengonversi biner menjadi bilangan desimal
+        char = chr(decimal_char)  # Mengonversi bilangan desimal ke karakter
+        text += char
+    return text
+
+def caesar_encrypt(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            encrypted_text += encrypted_char
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+
+# Memasukkan pesan terenkripsi dan kunci dari pengguna
 encrypted_message = input("Masukkan pesan terenkripsi: ")
-shift = int(input("Masukkan jumlah pergeseran yang digunakan saat enkripsi: "))
+shift = int(input("Masukkan jumlah pergeseran: "))
 
-# Mendapatkan kunci dari pergeseran Caesar
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-key = shifted_alphabet[0]
-for char in shifted_alphabet:
-    if shifted_alphabet.count(char) > shifted_alphabet.count(key):
-        key = char
-key_binary = format(ord(key), '08b')
+key = get_key_from_cesar(shift)
+key_qubit = most_frequent_bit(key)
 
-# Mendekripsi pesan menggunakan kunci dan sirkuit kuantum
-decrypted_message = decrypt_message(encrypted_message, key_binary)
+decrypted_message = decrypt_message(encrypted_message, key_qubit)
 
 # Mengonversi representasi biner ke dalam teks
 text = binary_to_text(decrypted_message)
 
 # Mendekripsi teks menggunakan Caesar Cipher
-decrypted_text = caesar_decrypt(text, shift)
+decrypted_text = caesar_encrypt(text, -shift)
 
-print("Teks terdekripsi:", decrypted_text)
+print("Pesan terdekripsi:", decrypted_text)
